40a642ae016abf23641c9565c863615d8a39d704
Hoo Sooyean 何書淵 <Sooyean.hoo@perforce.com>
2025-12-10 22:48:01 +0800

    ref : https://conveyor.hydraulic.dev/19.0/configs/hocon/ "All JSON is valid HOCON so........"
    Signed-off-by: Hoo Sooyean 何書淵 <Sooyean.hoo@perforce.com>

--- lib/facter/util/factercache.rb
+++ lib/facter/util/factercache.rb
@@ -4,6 +4,7 @@
 module Facter::Util
   require 'fileutils'
   require 'hocon'
+  require 'json'
 
   # Provides utilities for managing the Facter configuration file, including ensuring its existence,
   # adding TTLs for facts.
@@ -35,28 +36,34 @@ module Facter::Util
     private
 
     def puppetapplycmdttl(name, validation_seconds)
-      `mkdir -p -m 0644 $(dirname #{file_path}`
+      `mkdir -p -m 0644 $(dirname #{file_path})`
       facter_conf = Hocon.load(file_path)
       facter_conf['facts'] = { 'ttls' => [] } if facter_conf['facts'].nil?
       facter_conf['facts']['ttls'] = [] if facter_conf['facts']['ttls'].nil?
       facter_conf['facts']['ttls'] += [ {  name => "#{validation_seconds} seconds" } ]
+      facter_conf['facts']['ttls'].uniq!
 
-      puppetcode = <<-PUPPETCODE
-      $facts_ttls=#{facter_conf['facts']['ttls']}
-      hocon_setting { 'facter_conf.facts.ttls':
-        ensure  => present,
-        path    => '#{file_path}',
-        setting => 'facts.ttls',
-        type    => 'array',
-        value   => $facts_ttls,
-      }
-      PUPPETCODE
-      cmd = "#{Puppet.settings[:vardir].gsub(%r{cache$}, 'bin')}/puppet apply"
-      Open3.popen3(cmd) do |stdin, _stdout, _stderr, _wait|
-        stdin.puts(puppetcode)
-        stdin.close
-        # stdout.read
+      # ref : https://conveyor.hydraulic.dev/19.0/configs/hocon/
+      # "All JSON is valid HOCON so........"
+      File.open(file_path, 'w') do |f|
+        f.write(facter_conf.to_json)
       end
+      #      puppetcode = <<-PUPPETCODE
+      #      $facts_ttls=#{facter_conf['facts']['ttls']}
+      #      hocon_setting { 'facter_conf.facts.ttls':
+      #        ensure  => present,
+      #        path    => '#{file_path}',
+      #        setting => 'facts.ttls',
+      #        type    => 'array',
+      #        value   => $facts_ttls,
+      #      }
+      #      PUPPETCODE
+      #      cmd = "#{Puppet.settings[:vardir].gsub(%r{cache$}, 'bin')}/puppet apply"
+      #      Open3.popen3(cmd) do |stdin, _stdout, _stderr, _wait|
+      #        stdin.puts(puppetcode)
+      #        stdin.close
+      #        # stdout.read
+      #      end
     end
   end
 end
